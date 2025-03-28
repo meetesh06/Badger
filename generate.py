dcc7adf51e05db183167423f043da6097e683dd9
meetesh06 <meeteshmehta@cse.iitb.ac.in>
2025-03-28 12:25:48 +0530

    Dynamic sizing

--- generate.py
+++ generate.py
@@ -41,26 +41,61 @@ def plotBox(draw, rect, font_path, font_size, text_to_print):
 
 def plotForEntry(img, name1, aff1, rect, rect_affiliation, font_path):
     draw = ImageDraw.Draw(img)
-    max_font_size = 70
-    font_size = find_max_font_size(draw, name1, font_path, max_font_size, rect)
+    max_font_size = 68
+    min_font_size = 50  # Ensure font size is at least 50
     vertical_spacing = 15
-    if font_size < 60:
-        words = name1.split(" ")
-        total_text_height = sum(
-            ImageFont.truetype(font_path, font_size).getbbox(word)[3] for word in words
-        )
-        current_y = (rect[1] + rect[3] - total_text_height - (len(words) - 1) * vertical_spacing) // 2
-        for word in words:
-            font_size = 70
-            font = ImageFont.truetype(font_path, font_size)
-            bbox = draw.textbbox((0, 0), word, font=font)
+
+    # Split the name into lines if necessary
+    words = name1.split(" ")
+    lines = []
+    current_line = words[0]
+
+    for word in words[1:]:
+        test_line = f"{current_line} {word}"
+        font = ImageFont.truetype(font_path, max_font_size)
+        bbox = draw.textbbox((0, 0), test_line, font=font)
+        test_width = bbox[2] - bbox[0]
+        if test_width <= (rect[2] - rect[0]):
+            current_line = test_line
+        else:
+            lines.append(current_line)
+            current_line = word
+    lines.append(current_line)
+
+    # Find the maximum font size that works for all lines
+    for font_size in range(max_font_size, min_font_size - 1, -1):
+        font = ImageFont.truetype(font_path, font_size)
+        fits = True
+        for line in lines:
+            bbox = draw.textbbox((0, 0), line, font=font)
             text_width = bbox[2] - bbox[0]
-            text_height = bbox[3] - bbox[1]
-            text_position = ((rect[0] + rect[2] - text_width) // 2, current_y)
-            draw.text(text_position, word, font=font, fill="black")
-            current_y += text_height + vertical_spacing
-    else:
-        plotBox(draw, rect, font_path, font_size, name1)
+            if text_width > (rect[2] - rect[0]):
+                fits = False
+                break
+        if fits:
+            break  # Found the maximum font size that works for all lines
+
+    # Calculate total text height
+    total_text_height = len(lines) * font_size + (len(lines) - 1) * vertical_spacing
+    current_y = (rect[1] + rect[3] - total_text_height) // 2
+
+    # Draw each line
+    font = ImageFont.truetype(font_path, font_size)
+    for line in lines:
+        bbox = draw.textbbox((0, 0), line, font=font)
+        text_width = bbox[2] - bbox[0]
+        text_position = ((rect[0] + rect[2] - text_width) // 2, current_y)
+        draw.text(text_position, line, font=font, fill="black")
+        current_y += font_size + vertical_spacing
+
+    # Dynamically adjust rect_affiliation to be below the name rectangle
+    rect_affiliation[1] = rect[1] + total_text_height + vertical_spacing
+    rect_affiliation[3] = rect_affiliation[1] + 50  # Adjust height for affiliation text
+
+    # Ensure rect_affiliation does not overlap with the name rectangle
+    if rect_affiliation[1] < rect[3]:
+        rect_affiliation[1] = rect[3] + vertical_spacing
+        rect_affiliation[3] = rect_affiliation[1] + 50
 
     plotBox(draw, rect_affiliation, font_path, 50, aff1)
 
@@ -81,43 +116,43 @@ def main():
     outputs_path = args.out
     ensure_folder_exists_and_clear(outputs_path)
     df = pd.read_csv(csv_path)
-    namesss = df['Name']
-    addiliationnn = df['Affiliation']
+    names = df['Name']
+    affiliation = df['Affiliation']
 
-    for x in range(0, len(namesss), 6):
+    for x in range(0, len(names), 6):
         img = Image.open(template_path)
-        name1 = namesss[x]
-        aff1 = addiliationnn[x]
+        name1 = names[x]
+        aff1 = affiliation[x]
         rect = [1*horizontal_scaling, 4*vertical_scaling, 8.6*horizontal_scaling, 7.0*vertical_scaling]
         rect_affiliation = [1*horizontal_scaling, 6*vertical_scaling, 8.6*horizontal_scaling, 8.2*vertical_scaling]
         plotForEntry(img, name1, aff1, rect, rect_affiliation, font_path)
 
-        name1 = namesss[x + 1]
-        aff1 = addiliationnn[x + 1]
+        name1 = names[x + 1]
+        aff1 = affiliation[x + 1]
         rect = [1*horizontal_scaling, 14.5*vertical_scaling, 8.6*horizontal_scaling, 17.5*vertical_scaling]
         rect_affiliation = [1*horizontal_scaling, 16.5*vertical_scaling, 8.6*horizontal_scaling, 18.7*vertical_scaling]
         plotForEntry(img, name1, aff1, rect, rect_affiliation, font_path)
 
-        name1 = namesss[x + 2]
-        aff1 = addiliationnn[x + 2]
+        name1 = names[x + 2]
+        aff1 = affiliation[x + 2]
         rect = [11*horizontal_scaling, 4*vertical_scaling, 18.6*horizontal_scaling, 7*vertical_scaling]
         rect_affiliation = [11*horizontal_scaling, 6*vertical_scaling, 18.6*horizontal_scaling, 8.2*vertical_scaling]
         plotForEntry(img, name1, aff1, rect, rect_affiliation, font_path)
 
-        name1 = namesss[x + 3]
-        aff1 = addiliationnn[x + 3]
+        name1 = names[x + 3]
+        aff1 = affiliation[x + 3]
         rect = [11*horizontal_scaling, 14.5*vertical_scaling, 18.6*horizontal_scaling, 17.5*vertical_scaling]
         rect_affiliation = [11*horizontal_scaling, 16.5*vertical_scaling, 18.6*horizontal_scaling, 18.7*vertical_scaling]
         plotForEntry(img, name1, aff1, rect, rect_affiliation, font_path)
 
-        name1 = namesss[x + 4]
-        aff1 = addiliationnn[x + 4]
+        name1 = names[x + 4]
+        aff1 = affiliation[x + 4]
         rect = [21.1*horizontal_scaling, 4*vertical_scaling, 28.6*horizontal_scaling, 7*vertical_scaling]
         rect_affiliation = [21.1*horizontal_scaling, 6*vertical_scaling, 28.6*horizontal_scaling, 8.2*vertical_scaling]
         plotForEntry(img, name1, aff1, rect, rect_affiliation, font_path)
 
-        name1 = namesss[x + 5]
-        aff1 = addiliationnn[x + 5]
+        name1 = names[x + 5]
+        aff1 = affiliation[x + 5]
         rect = [21.1*horizontal_scaling, 14.5*vertical_scaling, 28.6*horizontal_scaling, 17.5*vertical_scaling]
         rect_affiliation = [21.1*horizontal_scaling, 16.5*vertical_scaling, 28.6*horizontal_scaling, 18.7*vertical_scaling]
         plotForEntry(img, name1, aff1, rect, rect_affiliation, font_path)
